386e586e7bc133fb19f459efb20e58d8c6a2d3c7
praveenn0904 <149926740+praveenn0904@users.noreply.github.com>
2025-05-07 13:09:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://medstock-backend-oymi.onrender.com',
+  origin: 'https://medstock1.netlify.app/',
   credentials: true
 }));
 app.use(express.json());
